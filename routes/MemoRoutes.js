daa55b81a41f887c66c89bb389307102e616c3fd
Tatjana Svjatova <tsv.mailbox@gmail.com>
2020-09-25 20:27:53 +0300

    async func Read route

--- routes/MemoRoutes.js
+++ routes/MemoRoutes.js
@@ -6,24 +6,24 @@ const Memo = require("../models/Memo.model");
 //CRUD
 //localhost:5000/memos and then at the end:
 //READ: => '/'
-memoRouter.get("/", (req, res) => {
-  Memo.find()
-    .then((memos) => res.status(200).json({ memos }))
-    // .catch((err) => res.status(400).json("Error: " + err));
-    .catch((err) =>
-      res.status(500).json({
-        message: {
-          msgBody: "Unable to get memosticks",
-          msgError: true,
-        },
-      })
-    );
+memoRouter.get("/", async (req, res) => {
+  try {
+    const memos = await Memo.find();
+    res.status(200).json({ memos });
+  } catch {
+    res.status(500).json({
+      message: {
+        msgBody: "Unable to get memosticks",
+        msgError: true,
+      },
+    });
+  }
 });
-
-// memoRouter.route("/").get((req, res) => {
+//// memoRouter.route("/").get((req, res) => {
+// memoRouter.get("/", (req, res) => {
 //   Memo.find()
 //     .then((memos) => res.status(200).json({ memos }))
-//     // .catch((err) => res.status(400).json("Error: " + err));
+//     // .catch((err) => res.status(400).json(`Error ${err}`));
 //     .catch((err) =>
 //       res.status(500).json({
 //         message: {
