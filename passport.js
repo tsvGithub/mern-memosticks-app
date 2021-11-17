851a84c499d5cdc40db45ceaa00e6d504a318d8e
Tatjana Svjatova <tsv.mailbox@gmail.com>
2021-11-17 12:46:47 +0200

    auth

--- passport.js
+++ passport.js
@@ -11,6 +11,73 @@ const User = require("./models/User.model");
 //A Passport strategy for AUTHORIZATION with a JSON Web Token.
 const JwtStrategy = require("passport-jwt").Strategy;
 
+//-----------------------
+//3
+//When 'user' is authenticated (logged in) we set a cookie on
+//the client browser and this cookie is the JWT token.
+//'cookieExtractor' extracts JWT token from the request ('req').
+
+//a)The client requests authorization to the authorization server.
+//b)When the authorization is granted, the authorization server
+//returns an access token to the application.
+//c)The application uses the access token to access a protected resource.
+const cookieExtractor = (req) => {
+  // extracted token:
+  let token = null;
+  //if there is req object & req.cookies not empty
+  if (req && req.cookies) {
+    //set extracted token to jwt access token from authorization server
+    token = req.cookies["access_token"];
+  }
+  //return 'null' or 'access token'
+  return token;
+};
+
+//----------------------
+//2
+//AUTHORIZATION (to protect endpoints) +++ (4c) Logout
+console.log(process.env.secretOrKey);
+
+passport.use(
+  //jwt strategy with options object
+  new JwtStrategy(
+    //1)first option 'jwtFromRequest' is the func (3) 'cookieExtractor'
+    //2)second option 'secretOrKey' is a secret key that we use to sign to token.
+    //'secretOrKey' is used to verify that the JWT token (from 'cookieExtractor') is valid.
+
+    //So we signed our JWT token with "process.env.secretOrKey".
+    //So we need to verify using the same 'key'.
+    //!!!Also the 'key' shouldn't be that simple.
+    {
+      jwtFromRequest: cookieExtractor,
+      //----------------------------
+      //matches (4ba) routes->User.js  process.env.secretOrKey,
+      //passport uses 'secretOrKey' to verify hat this token is legitimate (valid)
+      secretOrKey: process.env.secretOrKey,
+    },
+    //a verified callback:
+    //'payload' === data we set whithin JWT token
+    // & func 'done'
+    (payload, done) => {
+      //if user exists: find 'user'
+      // _id => search by 'primary key' in DB;
+      //JWT has 'claim' called 'subject'('sub')
+      //that is a 'primary key'('_id') of 'user'.
+      User.findById({ _id: payload.sub }, (err, user) => {
+        // if error: return 'done' func with Err & 'user' didn't find === false
+        if (err) return done(err, false);
+        //if user exists: return 'done' with err===null
+        //and 'user' object. Because 'user' is already authenticated (logged in)
+        //we don't need to check password to comapre
+        if (user) return done(null, user);
+        //if not user (that has a primary key===_id)
+        //return func done with err===null, user===false
+        else return done(null, false);
+      });
+    }
+  )
+);
+
 //---------------------
 //LOGIN => AUTHENTICATION
 //1) USE PASSPORT
