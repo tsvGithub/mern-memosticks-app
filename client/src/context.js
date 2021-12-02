f681215d5a9c087d112b7960ca0d4646d34032d7
Tatjana Svjatova <tsv.mailbox@gmail.com>
2021-12-02 13:09:33 +0200

    Auth Context

--- client/src/context.js
+++ client/src/context.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect, useContext } from "react";
 import axios from "axios";
+//Authentication (1)
+import AuthService from "./Services/AuthService";
 
 const AppContext = React.createContext();
 //THEME (1)
@@ -29,7 +31,17 @@ const AppProvider = ({ children }) => {
   const [timeOfDay, setTimeOfDay] = useState("");
   const [wish, setWish] = useState("");
   //user
-  const [user, setUser] = useState("Ted");
+  // const [user, setUser] = useState("Ted");
+  //--------------------------
+  //Authentication (2)
+  //'user'===user that is logged in
+  const [user, setUser] = useState(null);
+  //isAuthenticated===if this user is authenticated or not
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  //isLoaded is boolean value to see if the app is loaded
+  //once we get the data => isLoaded===true
+  const [isLoaded, setIsLoaded] = useState(false);
+  //-------------------------
   //THEME (2)
   const [mood, setMood] = useState(getStorageTheme());
   console.log(mood);
@@ -39,6 +51,40 @@ const AppProvider = ({ children }) => {
     setMood(mood === "dark" ? "light" : "dark");
     console.log(mood);
   };
+  //=================================
+  //Authentication (3)
+  //to persist authentication === сохранить аутентификацию
+  //once user is logeed in we set a 'state' to let App
+  //know that user has been authenticated.
+  //When you close React app that state is gone.
+  //So isAuthenticated syncs BE & FE together and
+  //when 'user' visits site again => stays logged in
+  useEffect(() => {
+    //use Services/AuthService.js func(1d) 'isAuthenticated'
+    AuthService.isAuthenticated()
+      //get back data
+      .then((data) => {
+        setIsLoaded(true);
+        // if authentication fails, Passport will respond with a 401 Unauthorized status,
+        //and any additional route handlers will not be invoked.
+        //If authentication succeeds, the 'req.user' property
+        //will be set to the authenticated user.
+        //Set 'user' state
+        setUser(data.user);
+        //-----------------------
+        // (BE 4b 'routes'->login)//if user is logged in
+        //isAuthenticated() added by Passport by default
+        //returns boolean (true/false)
+        //isAuthenticated: true because the user is successfully logged in
+        // {isAuthenticated: true,
+        ////send back user with username and role
+        //   user: { username, role },}------------------------
+        setIsAuthenticated(data.isAuthenticated);
+        //after getting data
+        setIsLoaded(false);
+      });
+  }, []);
+  //================================
   //THEME (4):
   //run every time 'mood' changes
   useEffect(() => {
@@ -98,26 +144,38 @@ const AppProvider = ({ children }) => {
   }, [time]);
   //========================================
   return (
-    <AppContext.Provider
-      value={{
-        times,
-        time,
-        setTime,
-        videos,
-        video,
-        timeOfDay,
-        wish,
-        user,
-        mood,
-        //functionalities:
-        switchMood,
-        chooseTimeInterval,
-        getVideos,
-        getOneVideo,
-      }}
-    >
-      {children}
-    </AppContext.Provider>
+    <div>
+      {isLoaded ? (
+        <h1>Loading...</h1>
+      ) : (
+        <AppContext.Provider
+          /*provide 'global state' to children===components*/
+          value={{
+            times,
+            time,
+            setTime,
+            videos,
+            video,
+            timeOfDay,
+            wish,
+            //Authentication (4)-----
+            user,
+            setUser,
+            isAuthenticated,
+            setIsAuthenticated,
+            //-------------
+            mood,
+            //functionalities:
+            switchMood,
+            chooseTimeInterval,
+            getVideos,
+            getOneVideo,
+          }}
+        >
+          {children}
+        </AppContext.Provider>
+      )}
+    </div>
   );
 };
 
