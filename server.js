1a6af9bbd04e9830acfc104c6c0716bf5c5907d5
Tatjana Svjatova <tsv.mailbox@gmail.com>
2022-01-26 17:47:57 +0200

    express-favicon

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const favicon = require("express-favicon");
 const app = express();
 
 const mongoose = require("mongoose");
@@ -60,6 +61,8 @@ require("dotenv").config();
 //(Deploying Step 1)
 // to test what enviroment that we're at
 if (process.env.NODE_ENV === "production") {
+  app.use(favicon(__dirname + "/build/favicon.ico"));
+  // the __dirname is the current directory from where the script is running
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
